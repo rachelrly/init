3a96e4aeb297dd19293a68e10a716c5807e440cd
Ady <ady@gms-macbook-pro.lan>
2020-12-02 10:05:17 -0800

    buzz feed implemented

---
+++ src/routes/BuzzRoute/BuzzRoute.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ActivityList from '../../components/ActivitiesList/ActivityList'
+
+
+class Buzz extends Component {
+    render() {
+        return (<>
+            <ActivityList />
+            </>
+        );
+    };
+};
+
+export default Buzz;
---
+++ src/services/activities-service.js
@@ -0,0 +1,22 @@
+import config from '../config';
+import TokenService from './token-service';
+
+const ActivitiesService = {
+
+    getUnreadActivity() {
+        return fetch(`${config.API_ENDPOINT}/activity`, {
+            headers: {
+                'Authorization': `bearer ${TokenService.getAuthToken()}`,
+
+            }
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    },
+
+}
+
+export default ActivitiesService
