f6debd81eae3251c9e9e0f9240aaf172aa6653a0
Rachel Reilly <r.a.reill18@gmail.com>
2020-12-01 18:46:34 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let apiPath
 let tokenKey
 
 if (process.env.NODE_ENV === 'production') {
-    apiPath = 'http://localhost:8000/api'
+    apiPath = 'https://init-team-api.herokuapp.com/api'
     tokenKey = 'init-client-auth-token'
 } else {
     apiPath = 'http://localhost:8000/api'
